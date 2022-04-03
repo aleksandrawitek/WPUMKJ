9eef8e7434d97a4743d6a44ef1eb01d263b99c4a
Aleksandra Witek <olciawitek@wp.pl>
2022-04-03 18:17:47 +0200

    Lista 2

--- Lista 2/zad3/src/main/kotlin/Main.kt
+++ Lista 2/zad3/src/main/kotlin/Main.kt
@@ -1,17 +1,48 @@
 fun main() {
-    fun isCyclic(i: String):Boolean {
+    fun isCyclic(i: String):Boolean
+    {
         //sprawdzenie czy naturalna
-        if (i.contains("-.,"))
+        if (i.contains("-") || i.contains(".") || i.contains(",")  )
         {
             println("Liczba nie jest naturalna, podaj prawidłową liczbę!")
             return false
         }
+
         else
         {
+            var length = i.length
+            var j = 1
+            while (j <= length)
+            {
+                var multiplicity = (i.toInt()*j).toString()
+                var mul_len = multiplicity.length
+                // każda wielokrotność liczby cyklicznek musi być tej samej długości co liczba wyjściowa
+
+                if (mul_len != length)
+                {
+                    return false
+                    break
+                }
+                else
+                {
+                    for (char in i)
+                    {
+                        val x = multiplicity.indexOf(char)
+                        // jesli cyfry nie ma to jej indeks wynosi -1
+                        if (x == -1)
+                        {
+                            return false
+                            break
+                        }
+                    }
+                }
+
+                j++
+            }
             return true
         }
     }
-    isCyclic("-430")
-    isCyclic("50.5")
-    isCyclic("400")
+    //println(isCyclic("-430"))
+    //println(isCyclic("50.5"))
+    println(isCyclic("142857"))
 }
